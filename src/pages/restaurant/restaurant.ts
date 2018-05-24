6d7eff4685c1165a33ab05e8c82af258c9356529
Giorgio Cristiano <cristianogiorgio1@gmail.com>
2018-05-24 03:29:32 +0200

    giorgio item interface

--- src/pages/restaurant/restaurant.ts
+++ src/pages/restaurant/restaurant.ts
@@ -7,6 +7,7 @@ import { ContactPage } from '../contact/contact';
 import {MenuPage} from '../menu/menu';
 
 @Component({
+  selector: 'page-restaurant'
   templateUrl: 'restaurant.html'
 })
 export class RestaurantPage {
